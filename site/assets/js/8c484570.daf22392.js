ce490cf2992715fafef10dad8ded4d0d464b9f6a
Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>
2024-11-28 11:54:03 +0100

    link to new docs page
    Also clean up lots of dead content. I really would have liked to do this
    properly, but the sources are not available. So this is just manually
    editing HTML files to get it looking like we want it.
    
    This page can hopefully be replaced with something more usable in the future.
    
    Signed-off-by: Moritz Wanzenböck <moritz.wanzenboeck@linbit.com>

--- site/assets/js/8c484570.daf22392.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[676],{2972:e=>{e.exports=JSON.parse('{"permalink":"/site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- site/assets/js/980b7d54.1e6565ff.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation=self.webpackChunkdocumentation||[]).push([[388],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
